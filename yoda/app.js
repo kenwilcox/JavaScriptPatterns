54fae81fae5df37ce068605e99c96c8b2968ab0f
Ken Wilcox <wilcoxk@medamerica.com>
2016-02-02 08:04:45 -0700

    Updated Yoda - sounds better

--- yoda/app.js
+++ yoda/app.js
@@ -1,3 +1,4 @@
 String.prototype.toYoda = function() {
-  return this.split(" ").sort().join(" ").replace("_", "").toUpperCase() + "!";
+  //return this.split(" ").sort().join(" ").replace("_", "").replace("~", "").toUpperCase() + "!";
+  return this.split(" ").sort().join(" ").replace(/_/g, "").replace(/~/g, "").toUpperCase() + "!";
 }
